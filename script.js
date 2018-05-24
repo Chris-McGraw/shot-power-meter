9033fd3a208b98f4eab43239bcd8190bac0bb63a
Chris McGraw <chris.rmcgraw1@gmail.com>
2018-05-23 22:55:11 -0400

    script-update-1
    initial addition of #power-indicator movement on spacebar press and #indicator-trail

--- script.js
+++ script.js
@@ -1,2 +1,24 @@
 $(document).ready(function() {
+
+/* ------------------------- Variable Declarations ------------------------- */
+  var $powerIndicator = $("#power-indicator");
+  var $indicatorTrail = $("#indicator-trail");
+
+  $(document).keypress(function(event) {
+    if(event.which === 32) {
+      $powerIndicator.addClass("power-indicator-move-right");
+      $indicatorTrail.addClass("expand-trail");
+
+      setTimeout(function() {
+        $powerIndicator.removeClass("power-indicator-move-right");
+        $powerIndicator.addClass("power-indicator-move-left");
+
+        setTimeout(function() {
+          $powerIndicator.removeClass("power-indicator-move-left");
+          $indicatorTrail.removeClass("expand-trail");
+        }, 1000);
+      }, 1000);
+    }
+  });
+
 });
