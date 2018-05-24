8f6c1e2718cd5b391ce3ebadba0d6983f121cf4b
Chris McGraw <chris.rmcgraw1@gmail.com>
2018-05-24 17:59:39 -0400

    script-update-3

--- script.js
+++ script.js
@@ -6,9 +6,11 @@ $(document).ready(function() {
   var $indicatorGhost = $("#indicator-ghost");
 
   var indicatorGhostPositionX = 0;
+  var spaceBarPress = 0;
 
-  $(document).keypress(function(event) {
-    if(event.which === 32) {
+  $(document).keydown(function(event) {
+    if(event.which === 32 && spaceBarPress === 0) {
+      spaceBarPress = 1;
       $powerIndicator.addClass("power-indicator-move-right");
       $indicatorTrail.addClass("expand-trail");
 
@@ -22,11 +24,13 @@ $(document).ready(function() {
           $indicatorTrail.css("width", "4px");
           $indicatorGhost.addClass("hidden");
           indicatorGhostPositionX = $powerIndicator.position().left;
+          spaceBarPress = 0;
         }, 1000);
       }, 1000);
     }
 
-    if(event.which === 13) {
+    else if(event.which === 32 && spaceBarPress === 1) {
+      spaceBarPress = 2;
       indicatorGhostPositionX = $powerIndicator.position().left;
       $indicatorTrail.removeClass("expand-trail");
       $indicatorTrail.css("width", indicatorGhostPositionX - 3 + "px");
