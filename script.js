2105c7bee9a057c079173122fded92562293be93
Chris McGraw <chris.rmcgraw1@gmail.com>
2018-05-24 23:05:19 -0400

    script-update-4

--- script.js
+++ script.js
@@ -10,16 +10,21 @@ $(document).ready(function() {
 
   $(document).keydown(function(event) {
     if(event.which === 32 && spaceBarPress === 0) {
-      spaceBarPress = 1;
       $powerIndicator.addClass("power-indicator-move-right");
       $indicatorTrail.addClass("expand-trail");
 
       setTimeout(function() {
-        $powerIndicator.removeClass("power-indicator-move-right");
-        $powerIndicator.addClass("power-indicator-move-left");
+        if(spaceBarPress === 2) {
+          $powerIndicator.removeClass("power-indicator-move-return");
+          $powerIndicator.addClass("power-indicator-move-left");
+        }
+        else {
+          $powerIndicator.removeClass("power-indicator-move-right");
+          $powerIndicator.addClass("power-indicator-move-return");
+        }
 
         setTimeout(function() {
-          $powerIndicator.removeClass("power-indicator-move-left");
+          $powerIndicator.removeClass("power-indicator-move-return");
           $indicatorTrail.removeClass("expand-trail");
           $indicatorTrail.css("width", "4px");
           $indicatorGhost.addClass("hidden");
@@ -27,15 +32,18 @@ $(document).ready(function() {
           spaceBarPress = 0;
         }, 1000);
       }, 1000);
+
+      spaceBarPress = 1;
     }
 
     else if(event.which === 32 && spaceBarPress === 1) {
-      spaceBarPress = 2;
       indicatorGhostPositionX = $powerIndicator.position().left;
       $indicatorTrail.removeClass("expand-trail");
       $indicatorTrail.css("width", indicatorGhostPositionX - 3 + "px");
       $indicatorGhost.css("left", indicatorGhostPositionX + "px");
       $indicatorGhost.removeClass("hidden");
+
+      spaceBarPress = 2;
     }
   });
 
