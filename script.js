77b29728c9771f83c7223298bb59bbc183034454
Chris McGraw <chris.rmcgraw1@gmail.com>
2018-05-30 13:05:23 -0400

    script-update-24
    edits to playerShot() functionality

--- script.js
+++ script.js
@@ -29,14 +29,14 @@ $(document).ready(function() {
 
         shotLength1 = -217 - (-7 * shotLoopCount1);
         $disc.addClass("disc-shot-1");
-        $disc.css({"transform": "translate(25px," + shotLength1 + "px) rotate(90deg)"});
+        $disc.css({"transform": "translate(20px," + shotLength1 + "px) rotate(90deg)"});
 
         console.log("shotLength1 = " + shotLength1);
 
         shadowMultiplier1 = Math.floor(shotLength1 * 0.15);
         shadowShotLength1 = (shotLength1 - shadowMultiplier1);
         $discShadow.addClass("disc-shadow-shot-1");
-        $discShadow.css({"transform": "translate(25px," + shadowShotLength1 + "px) rotate(90deg)"});
+        $discShadow.css({"transform": "translate(20px," + shadowShotLength1 + "px) rotate(90deg)"});
 
   /* ----- Shot 2 Functionality ----- */
         setTimeout(function() {
