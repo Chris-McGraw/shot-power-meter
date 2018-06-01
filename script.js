73fa85d4196f9eb1e8f8c6ea8126a02ad1c9a0e0
Chris McGraw <chris.rmcgraw1@gmail.com>
2018-06-01 11:15:37 -0400

    script-update-29
    edits to playerShot() function. initial additon of late-release-1 functionality

--- script.js
+++ script.js
@@ -19,6 +19,10 @@ $(document).ready(function() {
   var shotLoopCount1 = 0;
   var shotLoopCount2 = 0;
 
+  var releaseLoopValue = 0;
+  var releaseLoopCount1 = 0;
+  var releaseLoopCount2 = 0;
+
 /* ------------------------- Function Declarations ------------------------- */
 
   function playerShot() {
@@ -56,41 +60,50 @@ $(document).ready(function() {
           }, 1000);
         }
 
-        else if(releasePoint >= 64 && releasePoint <= 217) {
-          /* console.log("is " + shotPower + " greater than or equal to " + (shotLoopValue - 6));
-          console.log("and less than or equal to " + (shotLoopValue) + "?"); */
+        else if(releasePoint >= 10 && releasePoint <= 63) {
+          for(releaseLoopValue = 63; releaseLoopValue >= 15; releaseLoopValue -= 6) {
+            if(releasePoint >= (releaseLoopValue - 5) && releasePoint <= (releaseLoopValue)) {
 
-          shotLength1 = -217 - (-7 * shotLoopCount1);
-          $disc.addClass("disc-shot-1");
+              console.log("between: " + (releaseLoopValue - 5));
+              console.log("and: " + releaseLoopValue);
 
-          shotWidth1 = 20;
+              shotLength1 = -217 - (-7 * shotLoopCount1);
+              $disc.addClass("disc-shot-1");
 
-          $disc.css({"transform": "translate(" + shotWidth1 + "px," + shotLength1 + "px) rotate(90deg)"});
+              shotWidth1 = 80 - (6.5 * releaseLoopCount1);
 
-          console.log("shotLength1 = " + shotLength1);
+              $disc.css({"transform": "translate(" + shotWidth1 + "px," + shotLength1 + "px) rotate(90deg)"});
 
-          shadowMultiplier1 = Math.floor(shotLength1 * 0.15);
-          shadowShotLength1 = (shotLength1 - shadowMultiplier1);
-          $discShadow.addClass("disc-shadow-shot-1");
-          $discShadow.css({"transform": "translate(" + shotWidth1 + "px," + shadowShotLength1 + "px) rotate(90deg)"});
+              console.log("shotLength1 = " + shotLength1);
 
-    /* ----- Shot 2 Functionality ----- */
-          setTimeout(function() {
-            shotLength2 = -310 - (-10 * shotLoopCount2);
+              shadowMultiplier1 = Math.floor(shotLength1 * 0.15);
+              shadowShotLength1 = (shotLength1 - shadowMultiplier1);
+              $discShadow.addClass("disc-shadow-shot-1");
+              $discShadow.css({"transform": "translate(" + shotWidth1 + "px," + shadowShotLength1 + "px) rotate(90deg)"});
 
-            $disc.addClass("disc-shot-2");
+        /* ----- Shot 2 Functionality ----- */
+              setTimeout(function() {
+                shotLength2 = -310 - (-10 * shotLoopCount2);
 
-            shotWidth2 = 0;
+                $disc.addClass("disc-shot-2");
 
-            $disc.css({"transform": "translate(" + shotWidth2 + "px," + shotLength2 + "px) rotate(90deg)"});
+                shotWidth2 = 120 - (13 * releaseLoopCount2);
 
-            console.log("shotLength2 = " + shotLength2);
-            console.log("");
+                $disc.css({"transform": "translate(" + shotWidth2 + "px," + shotLength2 + "px) rotate(90deg)"});
 
-            shadowShotLength2 = shotLength2;
-            $discShadow.addClass("disc-shadow-shot-2");
-            $discShadow.css({"transform": "translate(" + shotWidth2 + "px," + shadowShotLength2 + "px) rotate(90deg)"});
-          }, 1000);
+                console.log("shotLength2 = " + shotLength2);
+                console.log("");
+
+                shadowShotLength2 = shotLength2;
+                $discShadow.addClass("disc-shadow-shot-2");
+                $discShadow.css({"transform": "translate(" + shotWidth2 + "px," + shadowShotLength2 + "px) rotate(90deg)"});
+              }, 1000);
+            }
+            releaseLoopCount1++;
+            setTimeout(function() {
+              releaseLoopCount2++;
+            }, 1000);
+          }
         }
 
       }
@@ -113,6 +126,10 @@ $(document).ready(function() {
 
       shotLoopCount1 = 0;
       shotLoopCount2 = 0;
+
+      releaseLoopCount1 = 0;
+      releaseLoopCount2 = 0;
+
     }, 3000);
   }
 
