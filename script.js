1d9cdfdbf1b160fd6671c8a7b6793a7b5d60ad9d
Chris McGraw <chris.rmcgraw1@gmail.com>
2018-06-02 09:56:24 -0400

    script-update-35
    script clean up

--- script.js
+++ script.js
@@ -25,39 +25,49 @@ $(document).ready(function() {
 
 /* ------------------------- Function Declarations ------------------------- */
 
+  function shotStep1() {
+    $disc.addClass("disc-shot-1");
+    $disc.css({"transform": "translate(" + shotWidth1 + "px," + shotLength1 + "px) rotate(90deg)"});
+
+    console.log("shotLength1 = " + shotLength1);
+    console.log("shotWidth1 = " + shotWidth1);
+
+    shadowMultiplier1 = Math.floor(shotLength1 * 0.15);
+    shadowShotLength1 = (shotLength1 - shadowMultiplier1);
+    $discShadow.addClass("disc-shadow-shot-1");
+    $discShadow.css({"transform": "translate(" + shotWidth1 + "px," + shadowShotLength1 + "px) rotate(90deg)"});
+  }
+
+  function shotStep2() {
+    $disc.addClass("disc-shot-2");
+    $disc.css({"transform": "translate(" + shotWidth2 + "px," + shotLength2 + "px) rotate(90deg)"});
+
+    console.log("shotLength2 = " + shotLength2);
+    console.log("shotWidth2 = " + shotWidth2);
+
+    shadowShotLength2 = shotLength2;
+    $discShadow.addClass("disc-shadow-shot-2");
+    $discShadow.css({"transform": "translate(" + shotWidth2 + "px," + shadowShotLength2 + "px) rotate(90deg)"});
+  }
+
   function playerShot() {
     for(shotLoopValue = 217; shotLoopValue > 0; shotLoopValue -= 7) {
       if(shotPower >= (shotLoopValue - 6) && shotPower <= (shotLoopValue)) {
       /* ------------ Good Release ------------ */
         if(releasePoint >= -9 && releasePoint <= 9) {
         /* ----- Shot Part 1 ----- */
+          console.log("Good Release!");
+
           shotLength1 = -217 - (-7 * shotLoopCount1);
           shotWidth1 = 20;
 
-          $disc.addClass("disc-shot-1");
-          $disc.css({"transform": "translate(" + shotWidth1 + "px," + shotLength1 + "px) rotate(90deg)"});
-
-          console.log("shotLength1 = " + shotLength1);
-
-          shadowMultiplier1 = Math.floor(shotLength1 * 0.15);
-          shadowShotLength1 = (shotLength1 - shadowMultiplier1);
-          $discShadow.addClass("disc-shadow-shot-1");
-          $discShadow.css({"transform": "translate(" + shotWidth1 + "px," + shadowShotLength1 + "px) rotate(90deg)"});
-
+          shotStep1();
         /* ----- Shot Part 2 ----- */
           setTimeout(function() {
             shotLength2 = -310 - (-10 * shotLoopCount2);
             shotWidth2 = 0;
 
-            $disc.addClass("disc-shot-2");
-            $disc.css({"transform": "translate(" + shotWidth2 + "px," + shotLength2 + "px) rotate(90deg)"});
-
-            console.log("shotLength2 = " + shotLength2);
-            console.log("");
-
-            shadowShotLength2 = shotLength2;
-            $discShadow.addClass("disc-shadow-shot-2");
-            $discShadow.css({"transform": "translate(" + shotWidth2 + "px," + shadowShotLength2 + "px) rotate(90deg)"});
+            shotStep2();
           }, 1000);
         }
 
@@ -66,35 +76,18 @@ $(document).ready(function() {
           for(releaseLoopValue = 63; releaseLoopValue >= 15; releaseLoopValue -= 6) {
             if(releasePoint >= (releaseLoopValue - 5) && releasePoint <= (releaseLoopValue)) {
         /* ----- Shot Part 1 ----- */
-              console.log("releaseLoopRange = " + (releaseLoopValue - 5) + " - " + releaseLoopValue);
+              console.log("releaseLoopRange = " + (releaseLoopValue - 5) + " & " + releaseLoopValue);
 
               shotLength1 = -217 - (-7 * shotLoopCount1);
               shotWidth1 = 80 - (6.5 * releaseLoopCount1);
 
-              $disc.addClass("disc-shot-1");
-              $disc.css({"transform": "translate(" + shotWidth1 + "px," + shotLength1 + "px) rotate(90deg)"});
-
-              console.log("shotLength1 = " + shotLength1);
-
-              shadowMultiplier1 = Math.floor(shotLength1 * 0.15);
-              shadowShotLength1 = (shotLength1 - shadowMultiplier1);
-              $discShadow.addClass("disc-shadow-shot-1");
-              $discShadow.css({"transform": "translate(" + shotWidth1 + "px," + shadowShotLength1 + "px) rotate(90deg)"});
-
+              shotStep1();
         /* ----- Shot Part 2 ----- */
               setTimeout(function() {
                 shotLength2 = -310 - (-10 * shotLoopCount2);
                 shotWidth2 = 120 - (13 * releaseLoopCount2);
 
-                $disc.addClass("disc-shot-2");
-                $disc.css({"transform": "translate(" + shotWidth2 + "px," + shotLength2 + "px) rotate(90deg)"});
-
-                console.log("shotLength2 = " + shotLength2);
-                console.log("");
-
-                shadowShotLength2 = shotLength2;
-                $discShadow.addClass("disc-shadow-shot-2");
-                $discShadow.css({"transform": "translate(" + shotWidth2 + "px," + shadowShotLength2 + "px) rotate(90deg)"});
+                shotStep2();
               }, 1000);
             }
             releaseLoopCount1++;
@@ -104,50 +97,31 @@ $(document).ready(function() {
           }
         }
 
-        /* ---------- Right Release 2 ---------- */
-          else if(releasePoint >= -27 && releasePoint <= -8) {
-            for(releaseLoopValue = -27; releaseLoopValue <= -12; releaseLoopValue += 5) {
-              if(releasePoint >= (releaseLoopValue) && releasePoint <= (releaseLoopValue + 4)) {
-          /* ----- Shot Part 1 ----- */
-                console.log("between: " + (releaseLoopValue + 4));
-                console.log("and: " + releaseLoopValue);
+      /* ---------- Right Release 2 ---------- */
+        else if(releasePoint >= -27 && releasePoint <= -8) {
+          for(releaseLoopValue = -27; releaseLoopValue <= -12; releaseLoopValue += 5) {
+            if(releasePoint >= (releaseLoopValue) && releasePoint <= (releaseLoopValue + 4)) {
+        /* ----- Shot Part 1 ----- */
+              console.log("releaseLoopRange = " + (releaseLoopValue + 4) + " & " + releaseLoopValue);
 
-                shotLength1 = -217 - (-7 * shotLoopCount1);
-                shotWidth1 = 67 - (13 * releaseLoopCount1);
+              shotLength1 = -217 - (-7 * shotLoopCount1);
+              shotWidth1 = 67 - (13 * releaseLoopCount1);
 
-                $disc.addClass("disc-shot-1");
-                $disc.css({"transform": "translate(" + shotWidth1 + "px," + shotLength1 + "px) rotate(90deg)"});
-
-                console.log("shotLength1 = " + shotLength1);
-                console.log("shotWidth1 = " + shotWidth1);
-
-                shadowMultiplier1 = Math.floor(shotLength1 * 0.15);
-                shadowShotLength1 = (shotLength1 - shadowMultiplier1);
-                $discShadow.addClass("disc-shadow-shot-1");
-                $discShadow.css({"transform": "translate(" + shotWidth1 + "px," + shadowShotLength1 + "px) rotate(90deg)"});
-
-          /* ----- Shot Part 2 ----- */
-                setTimeout(function() {
-                  shotLength2 = -310 - (-10 * shotLoopCount2);
-                  shotWidth2 = 94 - (26 * releaseLoopCount2);
-
-                  $disc.addClass("disc-shot-2");
-                  $disc.css({"transform": "translate(" + shotWidth2 + "px," + shotLength2 + "px) rotate(90deg)"});
-
-                  console.log("shotLength2 = " + shotLength2);
-                  console.log("shotWidth2 = " + shotWidth2);
-
-                  shadowShotLength2 = shotLength2;
-                  $discShadow.addClass("disc-shadow-shot-2");
-                  $discShadow.css({"transform": "translate(" + shotWidth2 + "px," + shadowShotLength2 + "px) rotate(90deg)"});
-                }, 1000);
-              }
-              releaseLoopCount1++;
+              shotStep1();
+        /* ----- Shot Part 2 ----- */
               setTimeout(function() {
-                releaseLoopCount2++;
+                shotLength2 = -310 - (-10 * shotLoopCount2);
+                shotWidth2 = 94 - (26 * releaseLoopCount2);
+
+                shotStep2();
               }, 1000);
             }
+            releaseLoopCount1++;
+            setTimeout(function() {
+              releaseLoopCount2++;
+            }, 1000);
           }
+        }
 
       }
       shotLoopCount1++;
